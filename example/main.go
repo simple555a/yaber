58a4545bbcea4f5c2f3d93ed00eebe575c18dfdd
A. Svensson <lmasvensson@gmail.com>
2016-04-22 12:34:07 +0200

    Update main example with new AssetDir func.

--- example/main.go
+++ example/main.go
@@ -7,18 +7,23 @@ import (
 )
 
 func main() {
-	files := []string{
-		"templates/aaa.txt",
-		"templates/empty_file",
-		"templates/hello",
+	fmt.Println("Get a specific file.")
+	path := "templates/hello"
+	body, e := assets.Asset(path)
+	if e != nil {
+		panic(e)
+	}
+	fmt.Printf("file %s (%d bytes)\n", path, len(body))
+	fmt.Println(string(body))
+
+	fmt.Println("Get all files with a specific path prefix (or no prefix).")
+	files, e := assets.AssetDir("templates/")
+	if e != nil {
+		panic(e)
 	}
 
-	for _, p := range files {
-		body, e := assets.Asset(p)
-		if e != nil {
-			panic(e)
-		}
-		fmt.Printf("file %s (%d bytes)\n", p, len(body))
+	for path, body := range files {
+		fmt.Printf("file %s (%d bytes)\n", path, len(body))
 		fmt.Println(string(body))
 	}
 }
