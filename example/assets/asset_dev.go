2f966733c4b455cfb8344576666db640bf71a076
A. Svensson <lmasvensson@gmail.com>
2016-05-03 18:23:52 +0200

    Update example assets.

--- example/assets/asset_dev.go
+++ example/assets/asset_dev.go
@@ -6,43 +6,43 @@
 package assets
 
 import (
-        "io/ioutil"
-        "path/filepath"
+	"io/ioutil"
+	"path/filepath"
 )
 
 func Asset(path string) ([]byte, error) {
-        return ioutil.ReadFile(path)
+	return ioutil.ReadFile(path)
 }
 
 func AssetDir(dir string) (map[string][]byte, error) {
-        list := make(map[string][]byte)
-        dirs := []string{dir}
+	list := make(map[string][]byte)
+	dirs := []string{dir}
 
-        for len(dirs) > 0 {
-                d := dirs[0]
-                dirs = dirs[1:]
-                files, e := ioutil.ReadDir(d)
-                if e != nil {
-                        return nil, e
-                }
+	for len(dirs) > 0 {
+		d := dirs[0]
+		dirs = dirs[1:]
+		files, e := ioutil.ReadDir(d)
+		if e != nil {
+			return nil, e
+		}
 
-                for _, f := range files {
-                        fpath := filepath.Join(d, f.Name())
+		for _, f := range files {
+			fpath := filepath.Join(d, f.Name())
 
-                        if f.IsDir() {
-                                dirs = append(dirs, fpath)
-                                continue
-                        }
-                        if !f.Mode().IsRegular() {
-                                continue
-                        }
+			if f.IsDir() {
+				dirs = append(dirs, fpath)
+				continue
+			}
+			if !f.Mode().IsRegular() {
+				continue
+			}
 
-                        fbody, e := ioutil.ReadFile(fpath)
-                        if e != nil {
-                                return nil, e
-                        }
-                        list[fpath] = fbody
-                }
-        }
-        return list, nil
+			fbody, e := ioutil.ReadFile(fpath)
+			if e != nil {
+				return nil, e
+			}
+			list[fpath] = fbody
+		}
+	}
+	return list, nil
 }
