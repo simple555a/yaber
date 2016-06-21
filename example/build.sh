1b7c46796a0a232da32b05438ca7f98623bdadff
A. Svensson <lmasvensson@gmail.com>
2016-06-21 18:53:34 +0200

    Update example.

--- example/build.sh
+++ example/build.sh
@@ -2,4 +2,4 @@
 
 # Example script for generating the asset files, inside the assets/ dir.
 
-go run ../cmd/yaber/main.go -out assets/assets templates/
+go run ../cmd/yaber/main.go -out assets/assets -public templates/
